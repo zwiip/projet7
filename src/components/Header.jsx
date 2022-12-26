97ca714acb25d0a24a798029889e0aaa3c4ec6cf
zwiip <astri.brugeilles@gmail.com>
2022-12-26 17:39:01 +0100

    travail contenu et style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,17 @@ import { Link } from 'react-router-dom'
  
 function Header() {
     return (
+    <header>
+        <div className='logo'>
+            <Link to="/">
+                <img src="../assets/images/logo.png" alt="logo Kasa" />
+            </Link>
+        </div>
         <nav>
             <Link to="/">Accueil</Link>
             <Link to="/About">A Propos</Link>
         </nav>
+    </header>
     )
 }
 
