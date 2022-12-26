bf131f0b133cfdf6396d7fc047f2c5ec1830184f
zwiip <astri.brugeilles@gmail.com>
2022-12-26 17:36:51 +0100

    debogage de l'affichage sur le navigateur

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import '../styles/Home.css';
+import Banner from '../components/banner'
+import '../assets/styles/sass/main.scss';
 
 function Home() {
   return (
     <div className="Home">
-     tadam
+      <div>
+        <Banner image="../assets/images/banner-home.png" />
+      </div>
+      <h1>Voici la page d'accueil</h1>
     </div>
   );
 }
