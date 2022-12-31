7fdf4091b843d66ab46d2be128fa7a318c2ac2ce
zwiip <astri.brugeilles@gmail.com>
2022-12-31 08:13:44 +0100

    tentative de débogage de ma route NotFound

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,35 +24,3 @@ function App() {
 }
 
 export default App;
-
-/*
-import React from 'react';
-//import ReactDOM from 'react-dom/client';
-import { createRoot } from 'react-dom/client';
-//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { BrowserRouter } from 'react-router-dom';
-import { Route, Routes } from 'react-router-dom';
-import './assets/styles/sass/main.scss';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import About from './pages/About';
-import Details from './pages/Details';
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(
-  <BrowserRouter>
-        <Header />
-          <Routes>
-              <Route path="/" element={< Home />} />
-              <Route path="/About" element={< About />} />
-              <Route path="/Details" element={< Details />} />
-              <Route element={< NotFound />} />
-          </Routes>
-          <Footer />
-  </BrowserRouter>
-)
-*/
