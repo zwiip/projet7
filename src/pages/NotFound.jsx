8811ba883a115eecdac8cc4075ff27b70bd4e9f1
zwiip <astri.brugeilles@gmail.com>
2023-01-04 11:43:37 +0100

    ajout du style et mise en page notfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ import '../assets/styles/sass/main.scss'
 function NotFound() {
 
   return (
-    <div id="notfound">
+    <div className='notfound'>
       <h1>404</h1>
       <p>Oups! La page que vous demandez n'existe pas.</p>
       <Link to="/">Retourner sur la page d'accueil</Link>
