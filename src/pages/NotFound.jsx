5a7ef4408c630af342b98eb4530c96dea3e8b2b2
zwiip <astri.brugeilles@gmail.com>
2023-01-04 11:11:55 +0100

    debogage affichage page notfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,15 +1,13 @@
-import { useRouteError } from "react-router-dom";
+import { Link } from 'react-router-dom'
 import '../assets/styles/sass/main.scss'
 
 function NotFound() {
-  const error = useRouteError();
-  console.error(error);
 
   return (
     <div id="notfound">
       <h1>404</h1>
       <p>Oups! La page que vous demandez n'existe pas.</p>
-      <link>Retourner sur la page d'accueil</link>
+      <Link to="/">Retourner sur la page d'accueil</Link>
     </div>
   );
 }
